e81489d49b3e261af999f059537b2e9bd7daa018
Sergey-Proskurnin <proskurnin@ua.fm>
2021-04-11 20:53:08 +0300

    Adds listener to select countries and checking the number of countries

--- src/js/fetch-api.js
+++ src/js/fetch-api.js
@@ -1,18 +1,57 @@
+
+import debounce from 'lodash.debounce'
 import API from './fetchCountries';
 import countriesCards from '../templates/markup-card.hbs';
 import countrieslist from '../templates/markup-list.hbs';
 
+
+
 const articleCards = document.querySelector('.body-card');
 const list = document.querySelector('.list-country');
+const input = document.querySelector('.form-control')
 
-function render() {
-  const obj = API.fetchCountries('ua').then(renderCountryCard);
+console.log(input);
+let form = ''
+
+function onSearch(e) {
+  e.preventDefault()
+  list.innerHTML = ''
+  articleCards.innerHTML = ''
+  form = e.target.value;
+  
+  
+console.log(form);
+  render(form)
+}
+
+input.addEventListener('input', debounce(onSearch, 1000));
+
+function render(form) {
+  if (form !== '') {
+    console.log(form);
+    API.fetchCountries(form)
+    .then(renderCountryCard)
+    .catch(onFetchError)
+    .finally(() => form ='');
+   
+    }
+}
+function onFetchError(error) {
+  alert('Not this country!');
 }
 
 function renderCountryCard(country) {
-  const markupCard = countriesCards(country);
-  const markupList = countrieslist(country);
-  articleCards.innerHTML = markupCard;
-  list.innerHTML = markupList;
+  console.log(country.length);
+  if(country.length === 1) {
+        const markupCard = countriesCards(country);
+     articleCards.innerHTML = markupCard;
+  }
+  if (country.length > 1 &&country.length < 10){
+    const markupList = countrieslist(country);
+    list.innerHTML = markupList;
+  }
+  if (country.length < 1 || country.length > 9){
+    alert("Enter correct country name")
+  }
 }
-render();
+
