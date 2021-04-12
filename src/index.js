329f3627a5e85ca1649cf2f3a5ce700ef3e3c0ea
Sergey-Proskurnin <proskurnin@ua.fm>
2021-04-13 02:22:32 +0300

    Adds Pnotify on project and catch for promise

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import './styles.css';
+import  '@pnotify/core/dist/BrightTheme.css' ;
 import './js/fetch-api';
-
--- src/js/fetch-api.js
+++ src/js/fetch-api.js
@@ -3,8 +3,8 @@ import debounce from 'lodash.debounce'
 import API from './fetchCountries';
 import countriesCards from '../templates/markup-card.hbs';
 import countrieslist from '../templates/markup-list.hbs';
-
-
+import {error} from '@pnotify/core'
+import '@pnotify/core/dist/PNotify.css'
 
 const articleCards = document.querySelector('.body-card');
 const list = document.querySelector('.list-country');
@@ -24,20 +24,22 @@ console.log(form);
   render(form)
 }
 
-input.addEventListener('input', debounce(onSearch, 1000));
+input.addEventListener('input', debounce(onSearch, 500));
 
 function render(form) {
   if (form !== '') {
-    console.log(form);
-    API.fetchCountries(form)
+     API.fetchCountries(form)
     .then(renderCountryCard)
     .catch(onFetchError)
     .finally(() => form ='');
    
     }
 }
-function onFetchError(error) {
-  alert('Not this country!');
+console.log(alert.text);
+function onFetchError() {
+ error ({
+    text: 'Not this country!'
+  });
 }
 
 function renderCountryCard(country) {
@@ -51,7 +53,10 @@ function renderCountryCard(country) {
     list.innerHTML = markupList;
   }
   if (country.length < 1 || country.length > 9){
-    alert("Enter correct country name")
+    error ({
+      text: 'Too many matches found. Please enter a more specific query!'
+    });
+    
   }
 }
 
